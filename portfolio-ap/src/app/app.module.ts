40b53b01a0df459ba402db5498632bd2d5ea11e3
MelGaray04 <95778741+MelGaray04@users.noreply.github.com>
2022-06-23 20:02:38 -0300

    sumamos experiencia y educación

--- portfolio-ap/src/app/app.module.ts
+++ portfolio-ap/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
     LogoAPComponent,
     SocialComponent,
     BannerComponent,
-    SobreMiComponent
+    SobreMiComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
