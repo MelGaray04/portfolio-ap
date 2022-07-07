138b34a9ba28220d146c9ed8ee53ed2f05226130
MelGaray04 <95778741+MelGaray04@users.noreply.github.com>
2022-07-07 04:12:49 -0300

    agregando data al back

---
+++ portfolio-ap/BackEnd/src/main/java/com/portfolio/mg/Interface/IPersonaService.java
@@ -0,0 +1,14 @@
+package com.portfolio.mg.Interface;
+
+import com.portfolio.mg.Entity.Persona;
+import java.util.List;
+
+public interface IPersonaService {
+  public List<Persona> getPersona();
+  
+  public void savePersona(Persona persona);
+  
+  public void deletePersona(Long id);
+  
+  public Persona findPersona(Long id);
+}
